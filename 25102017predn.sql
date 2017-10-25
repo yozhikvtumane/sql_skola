0e586a9f51aae0b8b6c5413002141e6133b673f5
yozhikvtumane <toshamatveeev@gmail.com>
2017-10-25 10:31:52 +0200

    Go

---
+++ 25102017predn.sql
@@ -0,0 +1,40 @@
+--soucet vsech platu
+--select sum(SALARY)
+--from EMPLOYEE
+
+--primerny plat
+--select avg(SALARY)
+--from EMPLOYEE
+
+--min plat
+--select min(SALARY) , max(SALARY) as NEJVIC , sum(SALARY), max(SALARY) - min(SALARY) , 8-5 as ROZSAH
+--from EMPLOYEE
+
+--pocet oddeleni
+--select count(*) , count(MNGR_NO) , count(*) - count(MNGR_NO) BEZ_MANAGERA
+--from DEPARTMENT
+
+--pocet aktivnych prodejcu
+--select distinct(SALES_REP)
+--from SALES
+
+--pocet aktivnych prodejcu
+--select count(distinct SALES_REP)
+--from SALES
+
+--vyber vsech zeme
+--select COUNTRY
+--from COUNTRY
+
+
+--vyber vsech zeme
+--select list(COUNTRY)
+--from COUNTRY
+
+--typy prodanych polozek
+--select ITEM_TYPE
+--from SALES
+
+--typy prodanych polozek
+select list( distinct ITEM_TYPE)
+from SALES
